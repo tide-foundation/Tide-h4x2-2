5f82449ac5c3e99e6527308aced82d17b7d241b1
Julio <julio@tide.org>
2023-01-25 14:04:32 +1000

    added docker support for simulator

--- H4x2-Simulator/H4x2-Simulator/Program.cs
+++ H4x2-Simulator/H4x2-Simulator/Program.cs
@@ -14,6 +14,14 @@ services.AddScoped<IOrkService, OrkService>();
 
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseCors(builder => {
     builder.AllowAnyOrigin();
     builder.AllowAnyMethod();

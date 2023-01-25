6c81646879ab4af2cfe51c3b29607eda563d10fe
Julio <julio@tide.org>
2023-01-25 11:59:14 +1000

    added tide enclave to ork

---
+++ H4x2-Node/H4x2-Node/wwwroot/js/main.js
@@ -0,0 +1,138 @@
+import { SimulatorFlow, SignUp, Point } from "../modules/H4x2-TideJS/index.js";
+
+(function ($) {
+    "use strict";
+    window.onload = getAllOrks();
+    
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+        
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+        if(input[1].value != input[2].value){
+            check = false;
+            alert('Passwords are not match!');
+        }
+        var values = $('#ork-drop-down').val(); //get the values from multiple drop down
+        if(values.length < 3){
+            check = false;
+            alert('You have to select 3 ork urls !');
+        }
+        if(check){
+            signup(input[0].value , input[1].value, input[3].value, values); 
+            //window.location.href = "../modules/H4x2-TideJS/test.html";
+        }
+        return false;
+    });
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });  
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+      
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).addClass('active');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).removeClass('active');
+            showPass = 0;
+        }
+        
+        
+    });
+
+    async function getAllOrks() {
+     
+        var config = {
+            urls: ["http://localhost:5001"],
+        }     
+        const flow = new SimulatorFlow(config);
+        const activeOrks = await flow.getAllOrks(); 
+       
+            var select = document.getElementById("ork-drop-down");
+            for(var i = 0; i < activeOrks.length; i++) {
+                var opt = activeOrks[i];
+                var el = document.createElement("option");
+                el.textContent = opt[1];
+                el.value = opt;
+                select.add(el);                       
+            }     
+    }
+
+    async function signup(user, pass, secretCode, selectedOrks) {
+        /**
+         * @type {[string, Point][]}
+         */
+        var orkUrls = [];
+        selectedOrks.forEach(element => {
+            const myArray = element.split(",");
+            orkUrls.push([myArray[2], Point.fromB64(myArray[3])]);
+        });
+
+        var config = {
+            orkInfo: orkUrls,
+            simulatorUrl: 'http://localhost:5062/',
+            vendorUrl: 'http://localhost:5231/'
+        }
+        
+        var signup = new SignUp(config);
+        await signup.start(user, pass, secretCode);
+    }
+
+})(jQuery);
+
---
+++ H4x2-Node/H4x2-Node/wwwroot/modules/H4x2-TideJS/Clients/NodeClient.js
@@ -0,0 +1,89 @@
+// 
+// Tide Protocol - Infrastructure for a TRUE Zero-Trust paradigm
+// Copyright (C) 2022 Tide Foundation Ltd
+// 
+// This program is free software and is subject to the terms of 
+// the Tide Community Open Code License as published by the 
+// Tide Foundation Limited. You may modify it and redistribute 
+// it in accordance with and subject to the terms of that License.
+// This program is distributed WITHOUT WARRANTY of any kind, 
+// including without any implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE.
+// See the Tide Community Open Code License for more details.
+// You should have received a copy of the Tide Community Open 
+// Code License along with this program.
+// If not, see https://tide.org/licenses_tcoc2-0-0-en
+//
+
+import Point from "../Ed25519/point.js"
+import ClientBase from "./ClientBase.js"
+
+export default class NodeClient extends ClientBase {
+    /**
+     * @param {string} url
+     */
+    constructor(url){
+        super(url)
+    }
+
+    /**
+     * @param {Point} point
+     * @param {string} uid 
+     * @returns {Promise<Point>}
+     */
+    async ApplyPRISM(uid, point){
+        const data = this._createFormData({'point': point.toBase64()})
+        const response = await this._post(`/Apply/Prism?uid=${uid}`, data)
+        if(response.ok){
+            const resp_obj = JSON.parse(await response.text());
+            return Point.fromB64(resp_obj.applied);
+        }
+        return Promise.reject("Node client: " + response.status); // should never get here
+    }
+
+    /**
+     * @param {string} authData
+     * @param {string} uid 
+     * @returns {Promise<string>}
+     */
+    async ApplyAuthData(uid, authData){
+        const data = this._createFormData({'authData': authData})
+        const response = await this._post(`/Apply/AuthData?uid=${uid}`, data)
+        if(response.ok){
+            const resp_obj = JSON.parse(await response.text());
+            return resp_obj.encryptedCVK;
+        }
+        return Promise.reject("Node client: " + response.status);
+    }
+
+    /**
+     * @param {Point} point
+     * @param {string} uid 
+     * @returns {Promise<[string, Point]>}
+     */
+    async CreatePRISM(uid, point){
+        const data = this._createFormData({'point': point.toBase64()})
+        const response = await this._post(`/Create/Prism?uid=${uid}`, data)
+        if(response.ok){
+            const resp_obj = JSON.parse(await response.text());
+            return [resp_obj.encryptedState, Point.fromB64(resp_obj.point)];
+        }
+        return Promise.reject("Node client: " + response.status);
+    }
+
+    /**
+     * @param {string} uid
+     * @param {Point} prismPub 
+     * @param {string} encryptedState 
+     * @returns {Promise<[string, string]>}
+     */
+    async CreateAccount(uid, prismPub, encryptedState){
+        const data = this._createFormData({'prismPub': prismPub.toBase64(), 'encryptedState': encryptedState})
+        const response = await this._post(`/Create/Account?uid=${uid}`, data);
+        if(response.ok){
+            const resp_obj = JSON.parse(await response.text());
+            return [resp_obj.encryptedCVK, resp_obj.signedUID]
+        }
+        return Promise.reject("Node client: " + response.status);
+    }
+}

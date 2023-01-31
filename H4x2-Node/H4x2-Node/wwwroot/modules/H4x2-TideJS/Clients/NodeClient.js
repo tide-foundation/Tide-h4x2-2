7e2727c759fd934f1ea1424a67d144642bff9b51
aranthana <aru2191@gmail.com>
2023-01-31 12:07:12 +1000

    Ork error

--- H4x2-Node/H4x2-Node/wwwroot/modules/H4x2-TideJS/Clients/NodeClient.js
+++ H4x2-Node/H4x2-Node/wwwroot/modules/H4x2-TideJS/Clients/NodeClient.js
@@ -33,15 +33,16 @@ export default class NodeClient extends ClientBase {
      */
     async ApplyPRISM(uid, point){
         const data = this._createFormData({'point': point.toBase64()})
+        var response;
         try{
-            const response = await this._post(`/Apply/Prism?uid=${uid}`, data)
-        
-            const responseData = await this._handleError(response, "Apply Prism");
-            const resp_obj = JSON.parse(responseData);
-            return Point.fromB64(resp_obj.applied);
+            response = await this._post(`/Apply/Prism?uid=${uid}`, data)
         }catch{
             return Promise.reject("You account's ORKs are down !")
         }
+        const responseData = await this._handleError(response, "Apply Prism");
+        const resp_obj = JSON.parse(responseData);
+        return Point.fromB64(resp_obj.applied);
+       
     }
 
     /**

e49e06839d204ddce406b8d3be1d3d3628528ab5
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2023-02-07 17:22:02 +1000

    ork handles simulator restarts + connection hang

--- H4x2-Node/startup.sh
+++ H4x2-Node/startup.sh
@@ -15,25 +15,49 @@ if [ -z "$Ed25519Key" ]; then # Checks if key exists
 	sig=$(tide-key sign $Ed25519Key $url)
 
 	# Wait for 7 seconds then submit rego. Simulator will check ORK endpoint for pub key
-	bash -c "sleep 7; curl --location --request POST 'https://h4x22simulator.azurewebsites.net/orks' --form 'orkName="$1"' --form 'orkUrl="$url"' --form 'signedOrkUrl="$sig"';" & 
+	bash -c "sleep 7; curl -s --location --request POST 'https://h4x22simulator.azurewebsites.net/orks' --form 'orkName="$1"' --form 'orkUrl="$url"' --form 'signedOrkUrl="$sig"';" & 
 
 else
 	echo "Key exists"
-	hash=$(echo $RANDOM | md5sum | head -c 20; echo;)
-	url="https://$hash.tunnel.tide.org"
-	sig=$(tide-key sign $Ed25519Key $url)
-	echo $url
-	bash -c "sleep 7; curl --location --request POST 'https://h4x22simulator.azurewebsites.net/orks/update' --form 'orkName="$1"' --form 'newOrkUrl="$url"' --form 'signedOrkUrl="$sig"';" & 
+	pub=$(tide-key public-key $Ed25519Key)
+	exists=$(curl -s --get 'https://h4x22simulator.azurewebsites.net/orks/exists' --data-urlencode "pub=$pub")
+
+	# Check if ork exists in simulator
+	if [ "$exists" == "true" ]; then
+		# Update ork url
+		hash=$(echo $RANDOM | md5sum | head -c 20; echo;)
+		url="https://$hash.tunnel.tide.org"
+		sig=$(tide-key sign $Ed25519Key $url)
+		echo $url
+		bash -c "sleep 7; curl -s --location --request POST 'https://h4x22simulator.azurewebsites.net/orks/update' --form 'orkName="$1"' --form 'newOrkUrl="$url"' --form 'signedOrkUrl="$sig"';" & 
+	elif [ "$exists" == "false" ]; then
+		# Register ork
+		hash=$(tide-key pubhash $Ed25519Key) # Hash key pub, use as sudomain in local tunnel
+		url="https://$hash.tunnel.tide.org"
+		sig=$(tide-key sign $Ed25519Key $url)
+		echo $url
+		bash -c "sleep 7; curl -s --location --request POST 'https://h4x22simulator.azurewebsites.net/orks' --form 'orkName="$1"' --form 'orkUrl="$url"' --form 'signedOrkUrl="$sig"';" & 
+	else
+		echo "Network error - Could not reach simulator at https://h4x22simulator.azurewebsites.net"
+		exit 1
+	fi
 fi
 
 # Connect to tunnel server
 #localtunnel --subdomain $hash -s https://tunnel.tide.org --port 80 --no-dashboard -c 1 http &
 lt --port 80 --host https://tunnel.tide.org --subdomain $hash &
 
-# Add cronjob to keep connection alive - request / every 10 min
-{ crontab -l; echo "*/10 * * * * curl $url"; } | crontab -
-crond
-
 # Start ORK
 priv=$(tide-key private-key $Ed25519Key)
-dotnet H4x2-Node.dll $priv
+dotnet H4x2-Node.dll $priv &
+
+sleep 60
+
+# Function is put here to ensure container restarts once LT connection is lost
+# 20 second timeout
+active=$(curl -s -m 20 $url/active)
+while  [ "$active" == "true" ]
+do
+  sleep 1200 # 20min
+  active=$(curl -s -m 20 $url/active)
+done # ORK has lost local tunnel connection, and will restart
